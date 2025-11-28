d62e37c8afc36392772bbe1b544862a8acdac7a6
Jenny3005 <jennyhoundon@gmail.com>
2025-11-28 08:24:14 +0100

    Ajout de la fonctionnalité cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,20 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        api: __DIR__.'/../routes/api.php', 
+        web: _DIR_.'/../routes/web.php',
+        commands: _DIR_.'/../routes/console.php',
+        api: _DIR_.'/../routes/api.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Activation de CORS pour Laravel 12
+        $middleware->append([
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
